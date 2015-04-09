f98272090b19471eb6f94119df596a25666935ce
byjk <jk@tut.by>
2015-04-09 23:19:01 +0300

    sdcard1 ok in cwm

--- device_miata.mk
+++ device_miata.mk
@@ -17,7 +17,7 @@ endif
 PRODUCT_COPY_FILES += \
     $(LOCAL_KERNEL):kernel
 
-$(call inherit-product, build/target/product/full.mk)
+$(call inherit-product, build/target/product/full_base_telephony.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 PRODUCT_NAME := full_miata
@@ -25,8 +25,8 @@ PRODUCT_DEVICE := miata
 PRODUCT_CHARACTERISTICS := phone
 
 
-PRODUCT_COPY_FILES := \
-    $(LOCAL_PATH)/fstab.miata:root/fstab.miata 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/fstab.qcom:root/fstab.qcom 
 
 
 # Screen density
